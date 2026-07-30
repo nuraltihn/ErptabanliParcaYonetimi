f90d9db28e93b8b7c4b0be07212cac00ef5a660f
ikbalfrat <ikbal.firat@icloud.com>
2026-07-30 15:09:08 +0300

    blazor eklendi

---
+++ ERPweb/Data/AppDbContextcs.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data;
+using ERPweb.Data;
+namespace ERPweb.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Users> Users { get; set; }
+        //public DbSet<Role> Roles { get; set; }
+        //public DbSet <Depo> Depolar { get; set; }
+        //public DbSet<Kategori> Kategoriler { get; set; }
+        //public DbSet<Müşteri> Müşteriler { get; set; }
+        //public DbSet<Parça> Parçalar{ get; set; }
+        //public DbSet<Raf> Raflar { get; set; }
+        //public DbSet<Siparis> Siparisler { get; set; }
+        //public DbSet<SiparisDetay> SiparisDetaylari { get; set; }
+        //public DbSet<StokHareket> StokHareketleri { get; set; }
+        //public DbSet<Tedarikci> Tedarikciler { get; set; }
+
+
+
+    }
+}
---
+++ ERPweb/Data/Users.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace ERPweb.Data
+   
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string? AdSoyad { get; set; }
+        [Required]
+        public string KulAd { get; set; } = string.Empty;
+        [Required]
+        public string Sifre { get; set; } = string.Empty;
+        public int? RolId { get; set; }
+        public string? Email { get; set; }
+        public bool? Aktifmi { get; set; }
+        public DateTime? OlusturmaTarih { get; set; }
+
+    }
+}
